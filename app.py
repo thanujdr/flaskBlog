bfa1df1f1543ea71fa9a78104a505323064c526c
thanujdr <thanujdr@hotmail.com>
2020-04-15 20:42:28 +0530

    database created. errors fixed with creating database

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2bd2601361ee7a151175024abc9bc935'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, dafault='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 

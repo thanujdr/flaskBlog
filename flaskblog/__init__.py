b0223e82a135c7438afa22da09917b722740f035
thanujdr <thanujdr@hotmail.com>
2020-04-19 01:19:51 +0530

    complete working backend framework for blog site with all planned features

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2bd2601361ee7a151175024abc9bc935'
@@ -11,5 +13,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'thanuj.dr@gmail.com'#os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = 'Wizard143'
+#app.config['MAIL_DEFAULT_SENDER'] = 'thanuj.dr@gmail.com'
+
+mail = Mail(app)
 
 from flaskblog import routes
